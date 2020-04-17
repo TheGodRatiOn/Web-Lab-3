a05c6757649f87df0860f03571d023949cdc1913
George <37272823+TheGodRatiOn@users.noreply.github.com>
2020-04-17 11:08:13 +0300

    inital commit

---
+++ task2.js
@@ -0,0 +1,6 @@
+// Реализуйте функцию sum
+
+function sum (values = []) { 
+  return this.reduce((acc, currentValue) => acc + currentValue, 0)
+}
+console.log(sum.call([5, 5, 5, 5, 5, 5, 5, 5, 5]));
